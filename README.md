0c69a206bda37f41a4b03feeae4067c2f279c2ea
iam-veeramalla <abhishek.veeramalla@gmail.com>
2024-07-08 10:34:16 +0530

    feat: web application in golang using net/http
    Signed-off-by: iam-veeramalla <abhishek.veeramalla@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# demo-app
-demo e2e cicd
+# Go Web Application
+
+This is a simple website written in Golang. It uses the `net/http` package to serve HTTP requests.
+
+## Running the server
+
+To run the server, execute the following command:
+
+```bash
+go run main.go
+```
+
+The server will start on port 8080. You can access it by navigating to `http://localhost:8080/courses` in your web browser.
+
+## Looks like this
+
+![Website](static/images/golang-website.png)
+
+
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	// Render the home html page from static folder
+	http.ServeFile(w, r, "static/home.html")
+}
+
+func coursePage(w http.ResponseWriter, r *http.Request) {
+	// Render the course html page
+	http.ServeFile(w, r, "static/courses.html")
+}
+
+func aboutPage(w http.ResponseWriter, r *http.Request) {
+	// Render the about html page
+	http.ServeFile(w, r, "static/about.html")
+}
+
+func contactPage(w http.ResponseWriter, r *http.Request) {
+	// Render the contact html page
+	http.ServeFile(w, r, "static/contact.html")
+}
+
+func main() {
+
+	http.HandleFunc("/home", homePage)
+	http.HandleFunc("/courses", coursePage)
+	http.HandleFunc("/about", aboutPage)
+	http.HandleFunc("/contact", contactPage)
+
+	http.ListenAndServe(":8080", nil)
+}
