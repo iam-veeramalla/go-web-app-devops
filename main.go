2e9a0e830286c917e9f4fe92925d8a11c9078e2c
KrishnaKiran <krishnakiranmuppa06@gmail.com>
2026-01-20 15:24:35 -0500

    modified the name in main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func coursePage(w http.ResponseWriter, r *http.Request) {
-	// Render the course html page krishna kiran
+	// Render the course html page krishna kirans
 	http.ServeFile(w, r, "static/courses.html")
 }
 
